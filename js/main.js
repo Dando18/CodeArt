a140f41eba5bfb008f07afb85cb4f8aca337828f
Daniel Nichols <dando18studios@gmail.com>
2020-04-30 22:26:49 -0400

    First Source Commit

---
+++ js/main.js
@@ -0,0 +1,193 @@
+let canva = null;
+let ctx = null;
+
+let color_functions = {};
+let globals = {};
+
+$(() => {
+
+    canva = document.getElementById('main-img');
+    ctx = canva.getContext('2d');
+
+    color_functions.red = (i, j) => { return 255; };
+    color_functions.green = (i, j) => { return 255; };
+    color_functions.blue = (i, j) => { return 255; };
+    color_functions.alpha = (i, j) => { return 255; };
+
+
+    $('#color-btn').on('click', renderImage);
+
+    $('textarea').attr('cols', '10');
+    $(document).delegate('textarea', 'keydown', function (e) {
+        /* from -- https://stackoverflow.com/a/6637396/3769237 */
+        let keyCode = e.keyCode || e.which;
+
+        if (keyCode == 9) {
+            e.preventDefault();
+
+            let start = this.selectionStart;
+            let end = this.selectionEnd;
+
+            $(this).val(
+                $(this).val().substring(0, start) +
+                '  ' +
+                $(this).val().substring(end)
+            );
+
+            this.selectionStart = start + 2;
+            this.selectionEnd = start + 2;
+        }
+    });
+    $('textarea').each(function () {
+        this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+    }).on('input', function () {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+
+    setPreset('Rainbow');
+
+    $('textarea').trigger('input');
+
+    /* fill in presets in select element */
+    for (let preset of PRESETS) {
+        $('#presets').append(
+            $('<option></option>')
+                .attr('value', preset.name)
+                .text(preset.name)
+        );
+    }
+    $('#presets').on('change', function () { setPreset(this.value); });
+    $('#presets').val('Rainbow');
+
+    $('#width-input').val(canva.width);
+    $('#height-input').val(canva.height);
+    $('#resize-btn').on('click', updateDimensions);
+
+});
+
+function get(color, i, j) {
+    let val = {};
+    try {
+        val.value = color_functions[color](i, j, canva.width, canva.height, globals);
+    } catch (error) {
+        val.error = `<strong>Error</strong> computing color '${color}' at pixel (${i}, ${j}).<br/>\n` +
+            `<strong>${error.name}</strong>:  ${error.message}`;
+    }
+    return val;
+}
+
+function renderImage(updateCode = true) {
+    notice('', 'clear');
+
+    if (updateCode) {
+        updateFunctions();
+    }
+
+    let img_data = ctx.getImageData(0, 0, canva.width, canva.height);
+
+    let clamp = (x, def = 255) => {
+        if (x === null) return def;
+        else if (Number.isNaN(x)) return def;
+        else {
+            x = Math.floor(x);
+            return (x < 0) ? 0 : (x > 255) ? 255 : x
+        }
+    }
+
+
+    let should_stop = false;
+    for (let i = 0; i < canva.height; i++) {
+        for (let j = 0; j < canva.width; j++) {
+
+            ['red', 'green', 'blue', 'alpha'].forEach((color, offset) => {
+
+                val = get(color, i, j);
+
+                if (val.error) {
+                    notice(val.error, 'error');
+                    should_stop = true;
+                    return;
+                }
+
+                img_data.data[4 * (i * canva.width + j) + offset] = clamp(val.value);
+            });
+
+            if (should_stop) break;
+        }
+        if (should_stop) break;
+
+    };
+
+    ctx.putImageData(img_data, 0, 0);
+}
+
+function updateFunctions() {
+
+    ['red', 'green', 'blue', 'alpha'].forEach(color => {
+
+        let text = $('#' + color + '-textarea').val();
+
+        try {
+            color_functions[color] = Function('i', 'j', 'width', 'height', 'globals', text);
+        } catch (error) {
+            console.log('error on color: ' + color);
+            notice(`<strong>Error</strong> in color ${color}.<br/>` +
+                `<strong>${error.name}</strong>:  ${error.message}`);
+        }
+    });
+
+}
+
+function setPreset(name) {
+    for (let preset of PRESETS) {
+        if (preset.name === name) {
+
+            ['red', 'green', 'blue', 'alpha'].forEach(color => {
+                $('#' + color + '-textarea').text(preset[color]);
+            });
+
+            if (preset.hasOwnProperty('preferredSize')) {
+                $('#width-input').val(preset.preferredSize.width);
+                $('#height-input').val(preset.preferredSize.height);
+                updateDimensions(false);
+            }
+
+            break;
+        }
+    }
+
+    renderImage();
+}
+
+function notice(message, type = 'ok') {
+    if (type === 'ok') {
+
+    } else if (type === 'error') {
+        $('#notice').html(message);
+        $('#notice').removeClass('no-notice ok-notice warning-notice');
+        $('#notice').addClass('error-notice');
+        $('#notice-container').show(100);
+    } else if (type === 'warning') {
+
+    } else if (type === 'clear') {
+        $('#notice').removeClass('ok-notice warning-notice error-notice');
+        $('#notice').addClass('no-notice');
+
+        $('#notice-container').hide(50);
+    }
+}
+
+function updateDimensions(redraw = true) {
+    let new_width = $('#width-input').val();
+    let new_height = $('#height-input').val();
+
+    $('#draw-progress').attr('max', new_height);
+
+    canva.width = new_width;
+    canva.height = new_height;
+
+    if (redraw) {
+        renderImage(false);
+    }
+}
---
+++ js/presets.js
@@ -0,0 +1,91 @@
+const PRESETS = [
+    {
+        name: 'Rainbow',
+        red: 'return 255 * (height - i)/height;',
+        green: 'return 255 * (width - j)/width;',
+        blue: 'return 255 * j/width;',
+        alpha: 'return 255;',
+        preferredSize: {
+            width: 512,
+            height: 512,
+        },
+    },
+    {
+        name: 'Digital Triangles',
+        red: 'return j ^ (i-j) ^ i;',
+        green: 'return (i - height)^2 + (j - width)^2;',
+        blue: 'return i ^ (i-j) ^ j',
+        alpha: 'return 255;',
+        preferredSize: {
+            width: 1024,
+            height: 1024,
+        },
+    },
+    {
+        /* FROM -- https://codegolf.stackexchange.com/a/35739/56964 */
+        name: 'Mandelbrot',
+        red: 'let a = 0, b = 0, c = 0, d = 0, n = 0;\n' +
+            'while (c + d < 4 && n < 880) {\n' +
+            '  b = 2*a*b + j*(8e-9) - 0.645411;\n' +
+            '  a = c - d + i*(8e-9) + 0.356888;\n\n' +
+            '  c = a * a;\n' +
+            '  d = b * b;\n' +
+            '  n += 1;\n' +
+            '}\n' +
+            'return 255 * Math.pow((n - 80) / 800, 3);',
+        green: 'let a = 0, b = 0, c = 0, d = 0, n = 0;\n' +
+            'while (c + d < 4 && n < 880) {\n' +
+            '  b = 2*a*b + j*(8e-9) - 0.645411;\n' +
+            '  a = c - d + i*(8e-9) + 0.356888;\n\n' +
+            '  c = a * a;\n' +
+            '  d = b * b;\n' +
+            '  n += 1;\n' +
+            '}\n' +
+            'return 255 * Math.pow((n - 80) / 800, 0.7);',
+        blue: 'let a = 0, b = 0, c = 0, d = 0, n = 0;\n' +
+            'while (c + d < 4 && n < 880) {\n' +
+            '  b = 2*a*b + j*(8e-9) - 0.645411;\n' +
+            '  a = c - d + i*(8e-9) + 0.356888;\n\n' +
+            '  c = a * a;\n' +
+            '  d = b * b;\n' +
+            '  n += 1;\n' +
+            '}\n' +
+            'return 255 * Math.pow((n - 80) / 800, 0.5);',
+        alpha: 'return 255;',
+        preferredSize: {
+            width: 1024,
+            height: 1024,
+        },
+    },
+    {
+        name: 'Colored Waves',
+        red: 'let wavelength = height*2;\n' +
+            'let amplitude = 255;\n' +
+            'let phaseshift = 0;\n\n' +
+            'return amplitude * Math.sin((2*Math.PI/wavelength)*i + phaseshift);',
+        green: 'let wavelength = (height+width)/4;\n' +
+            'let amplitude = 255;\n' +
+            'let phaseshift = 0;\n\n' +
+            'return amplitude * Math.sin((2*Math.PI/wavelength)*(i + j) + phaseshift);',
+        blue: 'let wavelength = width*2;\n' +
+            'let amplitude = 255;\n' +
+            'let phaseshift = 0;\n\n' +
+            'return amplitude * Math.sin((2*Math.PI/wavelength)*j + phaseshift);',
+        alpha: 'return 255;',
+        preferredSize: {
+            width: 512,
+            height: 512,
+        },
+    },
+    {
+        name: 'Colored Cuts',
+        red: 'return (i + j) / 4 % 256;',
+        green: 'return (i+ 2*j) / 4 % 256;',
+        blue: 'return (2*i + j) / 4 % 256;',
+        alpha: 'return 255;',
+        preferredSize: {
+            width: 1024,
+            height: 1024,
+        },
+    }
+];
